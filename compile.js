deb9b7b84011cdfa43fa53cffffacc11176921f4
Jatin Mehta <jatinmehta.jm@gmail.com>
2021-12-12 14:29:09 -0500

    Basic contract + Basic mocha test

--- compile.js
+++ compile.js
@@ -1,1 +1,25 @@
-// compile code will go here
+//Ok, so we need to compile the solidity code in inbox.sol
+//For that, we can't import the file as require('./contracts/Inbox.sol'); -->BAD!
+//Reason: Whenever we `require( )` a file in a node project, the node engine will
+//attempt to execute the content of that file as javascript code. Thus, it'll throw error
+//since it will find something else (solidity) instead of js code.
+
+
+//Since we can't `require` the inbox.sol file, we'll need to read
+//the contents out of our file from our filesystem. For that, we need
+// modules - path & fs (standard modules, so no need to `npm install`)
+ const path = require('path'); //guarantees correct path on windows/unix systems
+ const fs = require('fs');
+ const solc = require('solc');
+
+ //Lets define the path to inbox.sol.
+ //__dirname: a constant defined by node that gets set to current working dir (so gets us in ~/inbox)
+ //
+ const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+
+ const source = fs.readFileSync(inboxPath, 'utf8');
+
+
+ //1: Number of different contracts we are trying to compile
+ module.exports = solc.compile(source, 1).contracts[':Inbox'];
+
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,1 +1,19 @@
 // contract test code will go here
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3'); //it starts with a capital W since we are going to use a constructor and make instances of it
+//web3 is an instance of Web3 class
+const web3 = new Web3(ganache.provider());
+
+beforeEach(() => {
+    web3.eth.getAccounts().then(
+        fetchedAccounts => {
+            console.log(fetchedAccounts);
+        }
+    )
+})
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {})
+});
+
